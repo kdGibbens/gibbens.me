33375039b9846d58b7401934423ff6a00c4b3a9b
kdGibbens <gibbenskd@gmail.com>
2023-10-18 15:03:25 -0700

    remove some text

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -12,14 +12,14 @@ const ResumePage = () => {
     {
       company: "Driveway.com",
       description:
-        "Front-end Engineer for the online dealership web application Driveway.com. In my position, I worked across a variety of teams, but primarily my home team was for the Shopping Cart experience. We used a mix of technologies including React.js with Typescript interacting with Restful API services providing GET, PATCH, and POST endpoints. The cart experience consisted of complex paths that included being able to add trade-in vehicles, add dealership incentives, and calculate vehicle costs and taxes and fees for all 50 states and US territories. We provided buying options with Cash, Financing, and Leasing for all vehicle types for a variety of makes and models (New and Used). For Finance and Leasing, we collected information that would provide real-time predictions if the applicant would be approved. All of these features were being updated into a new cart experience that was building new components and services from start to finish.",
+        "Front-end Engineer for the online dealership web application Driveway.com. In my position, I worked across a variety of teams, but primarily my home team was for the Shopping Cart experience.",
       title: "Font-end Engineer",
       skills: ["react", "typescript", "TSS", "MUI", "Next"],
     },
     {
       company: "CloudBolt Software",
       description:
-        "Front-end Engineer for the cloud management company CloudBolt. In my position, I was the only Front-end Engineer on the main CloudBolt application. My responsibilities included UI development with Sass, Less, HTML5, Django templating, JavaScript (ES6+ and vanilla/jQuery), Vue.js, and leading the Front-end for the team as they moved to a more modern approach. I introduced Jest testing, WebPack bundling, and VueJS micro front-ends and continued to enhance and maintain the current UI.",
+        "Front-end Engineer for the cloud management company CloudBolt. In my position, I was the only Front-end Engineer on the main CloudBolt application. ",
       title: "Front-end Engineer",
       skills: [
         "Vue",
