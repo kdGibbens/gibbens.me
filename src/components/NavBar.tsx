7a54c7f70d621a466f79b18c9d75b14ec3e9fdf8
kdGibbens <gibbenskd@gmail.com>
2024-08-13 14:59:13 -0700

    style updates

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,6 @@
-"use client";
-import Link from "next/link";
-import { useSelectedLayoutSegment } from "next/navigation";
+'use client';
+import Link from 'next/link';
+import { useSelectedLayoutSegment } from 'next/navigation';
 
 type LinkType = {
   url: string;
@@ -11,28 +11,26 @@ type LinkType = {
 const NavBar = () => {
   const segment = useSelectedLayoutSegment();
   const links: LinkType[] = [
-    { url: "/", label: "About", id: 0 },
-    { url: "/experience", label: "Experience", id: 1 },
+    { url: '/', label: 'About', id: 0 },
+    { url: '/experience', label: 'Experience', id: 1 },
   ];
 
   return (
-    <header>
-      <nav>
-        <ul className="flex md:flex-col justify-evenly">
-          {links.map((link) => (
-            <Link
-              key={link.id}
-              href={link.url}
-              className={`text-slate-50  text-xl md:text-3xl ${
-                link.url === segment ? "text-slate-500" : "text-slate-50"
-              } hover:text-slate-300 pt-2 pb-2`}
-            >
-              {link.label}
-            </Link>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <nav>
+      <ul className="flex md:flex-col justify-evenly">
+        {links.map((link) => (
+          <Link
+            key={link.id}
+            href={link.url}
+            className={`text-slate-50  text-xl md:text-3xl ${
+              link.url === segment ? 'text-slate-500' : 'text-slate-50'
+            } hover:text-slate-300 pt-2 pb-2`}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
   );
 };
 
